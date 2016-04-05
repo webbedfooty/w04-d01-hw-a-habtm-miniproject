e7954a97570a8dad228fd202afa5194c737836d5
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-04-04 19:43:25 -0500

    updated

--- main.rb
+++ main.rb
@@ -1,4 +1,8 @@
 require_relative "config/environment.rb"
+require_relative "meetup.rb"
+require_relative "member.rb"
+require_relative "topic.rb"
+require_relative "type.rb"
 require "active_record"
 require "pry"
 require "yaml"
